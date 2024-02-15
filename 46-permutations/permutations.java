fd45c03bbe7d1f6d5c89e7a716fc5ce16c65fa86
spoidy-369 <149338486+spoidy-369@users.noreply.github.com>
2024-02-16 01:10:56 +0530

    Time: 1 ms (95.80%) | Memory: 44.6 MB (36.04%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,21 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        Map<Integer,Boolean> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(i,false);
-        }
+        boolean vis[] = new boolean[nums.length];
         help(nums,
             new ArrayList<>(),
             res,
-            map);
+            vis);
         return res;
     }
-    public void help(int nums[],List<Integer> st,List<List<Integer>> res,Map<Integer,Boolean> map){
+    public void help(int nums[],List<Integer> st,List<List<Integer>> res,boolean vis[]){
         for(int i=0;i<nums.length;i++){
-            if(map.get(i)==false){
+            if(!vis[i]){
                 st.add(nums[i]);
-                map.put(i,true);
-                help(nums,st,res,map);
+                vis[i] = true;
+                help(nums,st,res,vis);
                 st.remove(st.size()-1);
-                map.put(i,false);
+                vis[i] = false;
             }
         }
         if(st.size()==nums.length) res.add(new ArrayList<>(st));
